8252bc3270377764b7ee64ce9e803e478cb855f3
izik <izik@shimonimb.local>
2026-05-14 16:59:38 +0300

    Added all World Cup team flags

--- app/page.js
+++ app/page.js
@@ -1067,23 +1067,66 @@ const filteredAdminMatches = matches.filter((match) => {
 });
 const getFlagUrl = (team) => {
   const flags = {
-    Brazil: "https://flagcdn.com/w40/br.png",
-    Argentina: "https://flagcdn.com/w40/ar.png",
-    France: "https://flagcdn.com/w40/fr.png",
-    England: "https://flagcdn.com/w40/gb-eng.png",
-    Spain: "https://flagcdn.com/w40/es.png",
-    Germany: "https://flagcdn.com/w40/de.png",
-    Italy: "https://flagcdn.com/w40/it.png",
-    Portugal: "https://flagcdn.com/w40/pt.png",
-    Netherlands: "https://flagcdn.com/w40/nl.png",
-    Belgium: "https://flagcdn.com/w40/be.png",
-    Croatia: "https://flagcdn.com/w40/hr.png",
-    Uruguay: "https://flagcdn.com/w40/uy.png",
-    USA: "https://flagcdn.com/w40/us.png",
-    Mexico: "https://flagcdn.com/w40/mx.png",
-    Japan: "https://flagcdn.com/w40/jp.png",
-    "South Korea": "https://flagcdn.com/w40/kr.png",
-  };
+  Mexico: "https://flagcdn.com/w40/mx.png",
+  "South Africa": "https://flagcdn.com/w40/za.png",
+  "South Korea": "https://flagcdn.com/w40/kr.png",
+  "Czech Republic": "https://flagcdn.com/w40/cz.png",
+
+  Canada: "https://flagcdn.com/w40/ca.png",
+  "Bosnia & Herzegovina": "https://flagcdn.com/w40/ba.png",
+  Qatar: "https://flagcdn.com/w40/qa.png",
+  Switzerland: "https://flagcdn.com/w40/ch.png",
+
+  Brazil: "https://flagcdn.com/w40/br.png",
+  Morocco: "https://flagcdn.com/w40/ma.png",
+  Haiti: "https://flagcdn.com/w40/ht.png",
+  Scotland: "https://flagcdn.com/w40/gb-sct.png",
+
+  USA: "https://flagcdn.com/w40/us.png",
+  Paraguay: "https://flagcdn.com/w40/py.png",
+  Australia: "https://flagcdn.com/w40/au.png",
+  Turkey: "https://flagcdn.com/w40/tr.png",
+
+  Germany: "https://flagcdn.com/w40/de.png",
+  Curacao: "https://flagcdn.com/w40/cw.png",
+  "Ivory Coast": "https://flagcdn.com/w40/ci.png",
+  Ecuador: "https://flagcdn.com/w40/ec.png",
+
+  Netherlands: "https://flagcdn.com/w40/nl.png",
+  Japan: "https://flagcdn.com/w40/jp.png",
+  Sweden: "https://flagcdn.com/w40/se.png",
+  Tunisia: "https://flagcdn.com/w40/tn.png",
+
+  Belgium: "https://flagcdn.com/w40/be.png",
+  Egypt: "https://flagcdn.com/w40/eg.png",
+  Iran: "https://flagcdn.com/w40/ir.png",
+  "New Zealand": "https://flagcdn.com/w40/nz.png",
+
+  Spain: "https://flagcdn.com/w40/es.png",
+  "Cape Verde": "https://flagcdn.com/w40/cv.png",
+  "Saudi Arabia": "https://flagcdn.com/w40/sa.png",
+  Uruguay: "https://flagcdn.com/w40/uy.png",
+
+  France: "https://flagcdn.com/w40/fr.png",
+  Senegal: "https://flagcdn.com/w40/sn.png",
+  Iraq: "https://flagcdn.com/w40/iq.png",
+  Norway: "https://flagcdn.com/w40/no.png",
+
+  Argentina: "https://flagcdn.com/w40/ar.png",
+  Algeria: "https://flagcdn.com/w40/dz.png",
+  Austria: "https://flagcdn.com/w40/at.png",
+  Jordan: "https://flagcdn.com/w40/jo.png",
+
+  Portugal: "https://flagcdn.com/w40/pt.png",
+  "DR Congo": "https://flagcdn.com/w40/cd.png",
+  Uzbekistan: "https://flagcdn.com/w40/uz.png",
+  Colombia: "https://flagcdn.com/w40/co.png",
+
+  England: "https://flagcdn.com/w40/gb-eng.png",
+  Croatia: "https://flagcdn.com/w40/hr.png",
+  Ghana: "https://flagcdn.com/w40/gh.png",
+  Panama: "https://flagcdn.com/w40/pa.png",
+};
 
   return flags[team] || null;
 };
